ffcbbab7151336f28618a43a2fe412f7763cfe13
Mikyung Han <mhan@cs.utexas.edu>
2022-04-09 23:45:59 -0500

    .

--- testing.py
+++ testing.py
@@ -15,39 +15,43 @@ def prep_list(values):
 
 
 def check_ips_by_flows(ips_by_flows):
-    correct_hash = '\xd6u\x96a\x104!\x16\xd3u!\xa9C}`\xd5'
-    if check(prep_list(ips_by_flows), correct_hash):
+    correct_hash = b'\xd6u\x96a\x104!\x16\xd3u!\xa9C}`\xd5'
+    actual = prep_list(ips_by_flows).encode("utf-8")
+    if check(actual, correct_hash):
         print("Hashes match. Your ips_by_flows is correct.")
     else:
         print("Hashes do not match.  Check for bugs in your code.")
 
 
 def check_ips_by_volume(ips_by_volume):
-    correct_hash = 'oz\xdf\xf8\x1d\xd8G\xbe\x8d\\\xea\xef\xb7\xca$T'
-    if check(prep_list(ips_by_volume), correct_hash):
+    correct_hash = b'oz\xdf\xf8\x1d\xd8G\xbe\x8d\\\xea\xef\xb7\xca$T'
+    actual = prep_list(ips_by_volume).encode("utf-8")
+    if check(actual, correct_hash):
         print("Hashes match. Your ips_by_volume is correct.")
     else:
         print("Hashes do not match.  Check for bugs in your code.")
 
 
 def check_ports_by_flows(ports_by_flows):
-    correct_hash = '\x898\xb3yYT&\xfck\x0b\x94\xc1\x87\xde\xd7\xc9'
-    if check(prep_list(ports_by_flows), correct_hash):
+    correct_hash = b'\x898\xb3yYT&\xfck\x0b\x94\xc1\x87\xde\xd7\xc9'
+    actual = prep_list(ports_by_flows).encode("utf-8")
+    if check(actual, correct_hash):
         print("Hashes match. Your ports_by_flows is correct.")
     else:
         print("Hashes do not match.  Check for bugs in your code.")
 
 
 def check_ports_by_volume(ports_by_volume):
-    correct_hash = '\xec\xbbL\xda\xb5c[\xe0zz\x91\xe6\x8b\x86\x05\xc0'
-    if check(prep_list(ports_by_volume), correct_hash):
+    correct_hash = b'\xec\xbbL\xda\xb5c[\xe0zz\x91\xe6\x8b\x86\x05\xc0'
+    actual = prep_list(ports_by_volume).encode("utf-8")
+    if check(actual, correct_hash):
         print("Hashes match. Your ports_by_volume is correct.")
     else:
         print("Hashes do not match.  Check for bugs in your code.")
 
 def check_longest_aspath(longest_aspath):
-    correct_hash = '\xb1dd\x8dq\xbe\xf8\x8e\xd5\xfez\xb7q\x02\xca\xc3'
-    if check(longest_aspath, correct_hash):
+    correct_hash = b'\xb1dd\x8dq\xbe\xf8\x8e\xd5\xfez\xb7q\x02\xca\xc3'
+    if check(longest_aspath.encode("utf-8"), correct_hash):
         print("Hashes match. Your longest_aspath is correct.")
     else:
         print("Hashes do not match. Check for bugs in your code.")
